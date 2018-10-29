545db9fadd0c97a129f5758bbca38ce202538d2c
Dr Nic Williams <drnicwilliams@gmail.com>
2018-10-29 14:16:05 +1000

    refactor provision action into own file

--- pkg/broker/broker.go
+++ pkg/broker/broker.go
@@ -2,12 +2,10 @@ package broker
 
 import (
 	"context"
-	"errors"
 
 	"github.com/starkandwayne/cf-marketplace-servicebroker/pkg/cfconfig"
 
 	"code.cloudfoundry.org/lager"
-	cf "github.com/cloudfoundry-community/go-cfclient"
 	"github.com/pivotal-cf/brokerapi"
 )
 
@@ -32,43 +30,6 @@ func NewMarketplaceBrokerImpl(cf *cfconfig.Config, logger lager.Logger) (bkr *Ma
 // Services creates the data returned by this Broker API's GET /v2/catalog endpoint
 func (bkr *MarketplaceBrokerImpl) Services(ctx context.Context) (catalog []brokerapi.Service, err error) {
 	return Marketplace, nil
-}
-
-// Provision forwards on a service instance provision request to the backend Cloud Foundry API
-func (bkr *MarketplaceBrokerImpl) Provision(ctx context.Context, instanceID string, details brokerapi.ProvisionDetails, asyncAllowed bool) (spec brokerapi.ProvisionedServiceSpec, err error) {
-	bkr.Logger.Info("provision.start", lager.Data{"guid": instanceID})
-	req := cf.ServiceInstanceRequest{
-		Name:            instanceID,
-		ServicePlanGuid: details.PlanID,
-		SpaceGuid:       bkr.CF.SpaceGUID,
-		// Parameters:      details.RawParameters,
-	}
-
-	cfclient, err := bkr.CF.Client()
-	if err != nil {
-		return
-	}
-	svcInstance, err := cfclient.CreateServiceInstance(req)
-	if err != nil {
-		return
-	}
-
-	spec.DashboardURL = svcInstance.DashboardUrl
-	spec.IsAsync = svcInstance.LastOperation.State == "in progress"
-	spec.OperationData = "provision::" + svcInstance.Guid
-
-	bkr.Logger.Info("provision.end", lager.Data{
-		"guid":                  instanceID,
-		"async":                 spec.IsAsync,
-		"operation-data":        spec.OperationData,
-		"last-operation.status": svcInstance.LastOperation.State,
-	})
-
-	if svcInstance.LastOperation.State == "failed" {
-		err = errors.New("cf failed to provision: " + svcInstance.LastOperation.Description)
-	}
-
-	return
 }
 
 // LastOperation looks up readiness/failure of asynchronous operations
---
+++ pkg/broker/provision.go
@@ -0,0 +1,47 @@
+package broker
+
+import (
+	"context"
+	"errors"
+
+	"code.cloudfoundry.org/lager"
+	cf "github.com/cloudfoundry-community/go-cfclient"
+	"github.com/pivotal-cf/brokerapi"
+)
+
+// Provision forwards on a service instance provision request to the backend Cloud Foundry API
+func (bkr *MarketplaceBrokerImpl) Provision(ctx context.Context, instanceID string, details brokerapi.ProvisionDetails, asyncAllowed bool) (spec brokerapi.ProvisionedServiceSpec, err error) {
+	bkr.Logger.Info("provision.start", lager.Data{"guid": instanceID})
+	req := cf.ServiceInstanceRequest{
+		Name:            instanceID,
+		ServicePlanGuid: details.PlanID,
+		SpaceGuid:       bkr.CF.SpaceGUID,
+		// Parameters:      details.RawParameters,
+	}
+
+	cfclient, err := bkr.CF.Client()
+	if err != nil {
+		return
+	}
+	svcInstance, err := cfclient.CreateServiceInstance(req)
+	if err != nil {
+		return
+	}
+
+	spec.DashboardURL = svcInstance.DashboardUrl
+	spec.IsAsync = svcInstance.LastOperation.State == "in progress"
+	spec.OperationData = "provision::" + svcInstance.Guid
+
+	bkr.Logger.Info("provision.end", lager.Data{
+		"guid":                  instanceID,
+		"async":                 spec.IsAsync,
+		"operation-data":        spec.OperationData,
+		"last-operation.status": svcInstance.LastOperation.State,
+	})
+
+	if svcInstance.LastOperation.State == "failed" {
+		err = errors.New("cf failed to provision: " + svcInstance.LastOperation.Description)
+	}
+
+	return
+}
